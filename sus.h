acaf59762cb79294ba111bfb2e342c64ce118e55
larissalmaguiar <74932106+larissalmaguiar@users.noreply.github.com>
2021-04-14 01:21:51 -0300

    Update sus.h

--- sus.h
+++ sus.h
@@ -12,13 +12,13 @@
 //TADS
 //================================
 #include "sacak-lcp.h"
-//#include "kseq.h"
+
 
 #define lcp(i) ((i < n) ? (LCP[i]) : (0))
-#define PHI(i) ((ISA[i]<n-1) ? (phi[i]) : (i))
 
 
-bool iguais(int *v1, int *v2, int *v3, int tam);
+
+bool iguais(int *v1, int *v2, int tam);
 void inicializacao (int *SUS1, int *SUS, int *ISA, int *phi, uint_t SA[], int n);
 void construcaoPLCP(int *PLCP, int phi[], char Text[], int n, int *SUS2);
 void SUS_tradicional(int *SUS, uint_t SA[], int n, int_t LCP[]);
