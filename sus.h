c08a1a7b8f89a8735a8ebf97226918aedb60bc05
larissalmaguiar <74932106+larissalmaguiar@users.noreply.github.com>
2021-05-28 15:41:33 -0300

    Update sus.h

--- sus.h
+++ sus.h
@@ -19,10 +19,13 @@
 
 
 bool equal(int *v1, int *v2, int tam);
-void initialize(int *SUS, int *SUS1, int *ISA, int *phi, uint_t *SA, int n);
-void buildPLCP(int *PLCP, int *phi, char *Text, int n, int *SUS);
+//void initialize(int *SUS, int *SUS1, int *ISA, int *phi, uint_t *SA, int n);
+void buildPLCP(int *PLCP, int *PHI, char *Text, int n, int *ISA, uint_t *SA);
 void SUS_T(int *SUS, int n, int_t *LCP, uint_t *SA);
-void SUS_1(int *SUS, int *PHI, int n, int *PLCP);
-void SUS_2(int *SUS2, int n, int *PLCP, int *phi);
+void SUS_1(int *SUS, int *PHI, int n, int *PLCP, char *Text, int *ISA, uint_t *SA);
+void SUS_2(int *SUS2, int n, int *PLCP, int *PHI, int *ISA, char *Text, uint_t *SA);
+void isa(int *ISA, int n, uint_t *SA);
+void phi (int *PHI, int n, int *ISA, uint_t *SA);
 void print(uint_t *SA, int *SUS, char *Text, int n);
+void PLCPSUS(int *PLCP, int *PHI, char *Text, int n, int *ISA, uint_t *SA, int *SUS);
 #endif
