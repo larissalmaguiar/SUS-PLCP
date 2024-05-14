f5ac010558f6bdef0231bd131e892ce0e8fa3e31
larissalmaguiar <larissalm.aguiar@gmail.com>
2024-05-14 17:15:24 -0300

    fix .sh

--- dataset/covid.sh
+++ dataset/covid.sh
@@ -5,15 +5,15 @@ mkdir -p results
 for i in ../dataset/COVID/*; do 
 
     test=${i##*/}; 
-    date >> /SUS-PLCP/dataset/results/${test}
+    date >> results/${test}
     echo "####";
     echo ${test}; 
     ls -lah $i
 
     for j in {0..3}; do 
         echo ALG $j; 
-        ../main-sus $i -A $j -t >> /SUS-PLCP/dataset/results/${test};
-        echo "" >> /SUS-PLCP/dataset/results/${test};
+        ../main-sus $i -A $j -t >> results/${test};
+        echo "" >> results/${test};
         echo ""
     done;
 done;
